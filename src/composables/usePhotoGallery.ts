2b444985a27be014dab4fdfefd8f2976b54ba39d
beyerperez123@gmail.com <beyerperez123@gmail.com>
2024-05-13 11:35:40 -0600

    ⭐Avance 3

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -1,4 +1,29 @@
 const photos = ref<UserPhoto[]>([]);
+const convertBlobToBase64 = (blob: Blob) =>
+    new Promise ((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.readAsDataURL(blob);
+    });
+const savePicture = async(photo: Photo, fileName: string): Promise<UserPhoto> => {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    const base64Data = (await convertBlobToBase64(blob)) as string;
+
+    const saveFile = await Filesystem.writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: Directory.Data,
+    });
+
+    return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+    };
+};
 
 import { ref, onMounted, watch } from 'vue';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
@@ -21,10 +46,7 @@ export const usePhotoGallery = () => {
     };
 
     const fileName = Date.now() + '.jpeg';
-    const savedFileImage = {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-    };
+    const savedFileImage = await savePicture(photo, fileName);
 
     photos.value = [savedFileImage, ...photos.value];
 
